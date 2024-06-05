e949ef99cb98d4a18cfdee042a558fc98702c042
Carlos Thompson <carlosethompson@gmail.com>
2024-06-05 01:26:15 +0100

    feat: angular buzzfeed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizzComponent } from './core/components/quizz/quizz.component';
+import { HomeComponent } from './resources/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizzComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
